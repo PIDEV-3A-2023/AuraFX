e95bd5ee1c9b104d29e321dab3878975ec474821
mahmouddriss <mahmoud.driss@esprit.tn>
2023-05-02 21:21:06 +0200

    v1

---
+++ src/GUI/RegisterFXMLController.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+
+import entity.User;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import javafx.util.converter.IntegerStringConverter;
+import org.controlsfx.control.Notifications;
+import org.mindrot.jbcrypt.BCrypt;
+import service.UserService;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class RegisterFXMLController implements Initializable {
+
+    @FXML
+    private TextField nom;
+    
+    @FXML
+    private TextField prenom;
+
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField tel;
+
+    @FXML
+    private PasswordField mdp;
+
+    UserService us = new UserService();
+    @FXML
+    private TextField age;
+    @FXML
+    private DatePicker datenai;
+    @FXML
+    private TextField adresse;
+    @FXML
+    private ImageView imv;
+
+        int c;
+        int file;
+        File pDir;
+        File pfile;
+        String lien;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+            UnaryOperator<TextFormatter.Change> integerFilter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("-?([1-9][0-9]*)?")) { 
+                return change;
+            }
+            return null;
+        };
+            tel.setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, integerFilter));
+
+            c = (int) (Math.random() * (300000 - 2 + 1)) + 2;
+            pDir = new File("src/gui/img/user" + c + ".jpg");
+            lien = "user" + c + ".jpg";
+
+            
+        
+    }    
+
+    @FXML
+    private void Enregistrer(ActionEvent event) {
+        
+         User u = new User ();
+         
+                  if( nom.getText().equals("")  || prenom.getText().equals("") || email.getText().equals("") || mdp.getText().equals("") || tel.getText().equals("0") || pfile == null|| adresse.getText().equals("") ){
+                                        Notifications.create().title("warnnig").text("Vérifier vos Cordonnée").position(Pos.BOTTOM_RIGHT).showError();
+;}
+             else if(us.checkEmailExist(email.getText())){
+                                            Notifications.create().title("warnnig").text("l'email exite déja").position(Pos.BOTTOM_RIGHT).showError();
+;
+            }
+             else if (us.validerEmail(email.getText())) {
+                                         Notifications.create().title("warnnig").text("valider Email").position(Pos.BOTTOM_RIGHT).showError();
+;
+             }
+                          else if (tel.getText().length()!=8) {
+                                         Notifications.create().title("warnnig").text("valider Votre numéro").position(Pos.BOTTOM_RIGHT).showError();
+;
+             }
+             else{
+        Date dd = new java.sql.Date(new Date(datenai.getEditor().getText()).getTime());
+        u.setNom(nom.getText());
+        u.setEmail(email.getText());
+        u.setPrenom(prenom.getText());
+        u.setTel(Integer.parseInt(tel.getText()));
+        copier(pfile, pDir);
+        u.setImage(lien);
+        u.setAdresse(adresse.getText());
+        u.setEmail(email.getText());
+        u.setDatenai(dd);
+        u.setIsavtive(1);
+        String pass = mdp.getText();
+        pass = BCrypt.hashpw(pass, BCrypt.gensalt(13));
+        pass= pass.replaceFirst("a", "y");
+        u.setMdp(pass);
+        u.setRole("[\"ROLE_USER\"]");
+        us.Registre(u);
+        
+                          }
+        
+    }
+
+    @FXML
+    private void Upload(ActionEvent event) throws MalformedURLException {
+        
+                         FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisir une image: ");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("JPEG", "*.jpeg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png"),
+                new FileChooser.ExtensionFilter("BMP", "*.bmp")
+        );
+        Window stage = null;
+        pfile = fileChooser.showOpenDialog(stage);
+
+        /* - draw image */
+        if (pfile != null) {
+            file=1;
+            Image image = new Image(pfile.toURI().toURL().toExternalForm());
+            imv.setImage(image);
+    }
+        
+    }
+    
+              public static boolean copier(File source, File dest) {
+        try (InputStream sourceFile = new java.io.FileInputStream(source);
+                OutputStream destinationFile = new FileOutputStream(dest)) {
+            // Lecture par segment de 0.5Mo  
+            byte buffer[] = new byte[512 * 1024];
+            int nbLecture;
+            while ((nbLecture = sourceFile.read(buffer)) != -1) {
+                destinationFile.write(buffer, 0, nbLecture);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false; // Erreur 
+        }
+        return true; // Résultat OK   
+    }
+    
+}
---
+++ src/services/UserService.java
@@ -0,0 +1,381 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+import entity.User;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import utils.DataSource;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UserService {
+ 
+    private final Connection cnx;
+    private static UserService instance;
+    
+    public UserService() {
+        cnx = DataSource.getInstance().getCnx();
+    }
+    
+    public static UserService getInstance()
+    {
+        if (instance == null) {
+            instance = new UserService();
+        }
+        return instance; 
+    }
+
+    
+    
+        public boolean Registre(User user) {
+        int verf = 0 ;
+        try{
+        String req ;
+        
+        req="INSERT INTO `user`(`nom`,`prenom`,`email`,`roles`,`password`,`date_nais`,`photo`,`tel`,`adresse`,`is_active`) VALUES (?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement res=cnx.prepareStatement(req);
+        
+        res.setString(1, user.getNom());
+        res.setString(2, user.getPrenom());
+        res.setString(3, user.getEmail());
+        res.setString(4, user.getRole());
+        res.setString(5, user.getMdp());
+        res.setDate(6, (Date) user.getDatenai());
+        res.setString(7, user.getImage());  
+        res.setLong(8, user.getTel());
+        res.setString(9, user.getAdresse());
+        res.setLong(10, user.getIsavtive());
+        verf=res.executeUpdate();
+         
+        
+        }
+        catch(SQLException e ){
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE,null,e);
+        
+        }
+        if (verf==0)
+        {return false;}
+        else {return true;}
+    }
+        
+   public User Login(String email) {
+
+        User user = new User();
+
+        try {
+            String requete = "select * from user where email='"+email+"'";
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(requete);
+            int count = 0;
+            while(rs.next()){
+                count ++;
+                user.setId(rs.getInt("id"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("roles"));
+                user.setDatenai(rs.getDate("date_nais"));
+                user.setMdp(rs.getString("password"));
+                user.setTel(rs.getInt("tel"));
+                user.setAdresse(rs.getString("adresse"));
+                user.setImage(rs.getString("photo"));
+                user.setIsavtive(rs.getInt("is_active"));
+
+            }
+            
+            if(count == 0){
+             return null ;
+            }else{
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
+    }
+   
+           public boolean modifieUser(User user) throws SQLDataException {
+               
+                String query = "UPDATE `user` SET `email`=?,`roles`=?,`nom`=?,`prenom`=?,`date_nais`=?,`tel`=?,`photo`=?,`adresse`=?,`is_active`=? WHERE `id` = ?";
+		PreparedStatement res;
+        try {
+                res = cnx.prepareStatement(query);
+        res.setString(3, user.getNom());
+        res.setString(4, user.getPrenom());
+        res.setString(1, user.getEmail());
+        res.setString(2, user.getRole());
+        res.setDate(5, (Date) user.getDatenai());
+        res.setString(7, user.getImage());  
+        res.setLong(6, user.getTel());
+        res.setString(8, user.getAdresse());
+        res.setInt(9, user.getIsavtive());
+        res.setInt(10, user.getId());
+                res.executeUpdate();
+                return true;
+                
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+           
+           
+    public boolean blouer(User user) throws SQLDataException {
+               
+                String query = "UPDATE `user` SET `is_active`=? WHERE `id` = ?";
+		PreparedStatement res;
+        try {
+                res = cnx.prepareStatement(query);
+
+        res.setInt(1,0);
+        res.setInt(2, user.getId());
+                res.executeUpdate();
+                return true;
+                
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+        public boolean deblouer(User user) throws SQLDataException {
+               
+                String query = "UPDATE `user` SET `is_active`=? WHERE `id` = ?";
+		PreparedStatement res;
+        try {
+                res = cnx.prepareStatement(query);
+
+        res.setInt(1,1);
+        res.setInt(2, user.getId());
+                res.executeUpdate();
+                return true;
+                
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    public boolean checkEmailExist(String email)
+    {
+        
+        int count = 0;
+           
+        String requete="select * from user where email='"+email+"' ";
+        try{
+            Statement st = cnx.createStatement();
+            ResultSet rsl = st.executeQuery(requete);
+            while(rsl.next())
+            {
+                count++;
+            }
+           if(count == 0){
+                return false;
+           }else{
+               return true;
+           }  
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+   }
+    
+    
+   public boolean validerEmail(String s){
+    Pattern p = Pattern.compile("[a-zA-Z0-9][a-zA-Z0-9._]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+");
+    Matcher m = p.matcher(s);
+    if (m.find() && m.group().equals(s)){
+        return false;
+    }
+    else 
+    {
+      
+        return true;
+    
+        
+}
+   }
+   
+    public List<User> getAllUser(){
+        
+        List<User> list = new ArrayList<User>();
+        int count =0;
+        
+        String requete="select * from user";
+         try{
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(requete);
+            
+            while (rs.next()){
+                
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("roles"));
+                user.setDatenai(rs.getDate("date_nais"));
+                user.setMdp(rs.getString("password"));
+                user.setTel(rs.getInt("tel"));
+                user.setAdresse(rs.getString("adresse"));
+                user.setImage(rs.getString("photo"));
+                user.setIsavtive(rs.getInt("is_active"));
+ 
+                
+                list.add(user);
+                
+                count++;
+            }
+            if(count == 0){
+                return null;
+           }else{
+               return list;
+            
+           
+        }
+         }
+        catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+   
+           
+}
+    
+       
+    public User findUserById(int id){
+        
+        User user = new User();
+        int count =0;
+        
+        String requete="select * from user where id="+id;
+         try{
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(requete);
+            
+            while (rs.next()){
+                
+                user.setId(rs.getInt("id"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("roles"));
+                user.setDatenai(rs.getDate("date_nais"));
+                user.setMdp(rs.getString("password"));
+                user.setTel(rs.getInt("tel"));
+                user.setAdresse(rs.getString("adresse"));
+                user.setImage(rs.getString("photo"));
+                user.setIsavtive(rs.getInt("is_active"));             
+                count++;
+            }
+            if(count == 0){
+                return null;
+           }else{
+               return user;
+            
+           
+        }
+         }
+        catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+   
+           
+}
+       
+           
+        public boolean deleteUser(int id) throws SQLDataException {
+        try {
+            
+            Statement st=cnx.createStatement();
+            String req= "DELETE FROM `user` WHERE `id_c` ="+id;
+            st.executeUpdate(req);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }    
+        }
+        
+        
+         public User findUserByEmail(String email)
+    {
+        User user = new User();
+        
+           
+        String requete="select * from user where email='"+email+"' ";
+        try{
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(requete);
+            while(rs.next())
+            {  
+                
+               user.setId(rs.getInt("id"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("roles"));
+                user.setDatenai(rs.getDate("date_nais"));
+                user.setMdp(rs.getString("password"));
+                user.setTel(rs.getInt("tel"));
+                user.setAdresse(rs.getString("adresse"));
+                user.setImage(rs.getString("photo"));
+                user.setIsavtive(rs.getInt("is_active"));  
+                
+            }
+           
+               
+            
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return user;
+   }
+        public boolean modifiePassword(User d) throws SQLDataException {
+               
+                String query = "UPDATE `user` SET `password`=? WHERE `id` = ?";
+		PreparedStatement st;
+        try {
+                st = cnx.prepareStatement(query);
+                st.setString(1,d.getMdp());
+                st.setInt(2,d.getId());
+                st.executeUpdate();
+                return true;
+                
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+        
+        
+}
