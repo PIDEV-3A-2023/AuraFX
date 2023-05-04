23c7ab793236677afdad2a12ab499b2671bc40a3
azer775 <azer.bennasr@esprit.tn>
2023-05-04 13:22:01 +0100

    d

--- src/GUI/BaseController.java
+++ src/GUI/BaseController.java
@@ -8,8 +8,6 @@ package GUI;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -19,8 +17,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import services.UserService;
-import utils.Sessions;
 
 /**
  * FXML Controller class
@@ -53,88 +49,35 @@ public class BaseController implements Initializable {
     private HBox GestionAffectationButon;
     @FXML
     private HBox GestionUserButon;
-    public static BaseController baseController;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-          UserService us = new UserService();
-//        currentUser = us.AfficherU();
-        ConnectedUserNameLabel.setText(Sessions.getLoggedInUser().getNom());
-        baseController = this;
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("afficherproduit.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Evenement");
-            gestionProduitButton.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
-        } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        // TODO
     }    
-    
-     private void removeSelectedStyle() {
-      //  gestionProduitButton.getStyleClass().clear();
-        GestionUserButon.getStyleClass().clear();
-        gestionProduitButton.getStyleClass().clear();
-        GestionCategorieButon.getStyleClass().clear();
-       // GestionCategorieButon.getStyleClass().clear();
-      //  GestionPostButon.getStyleClass().clear();
-      //  GestionCommentButon.getStyleClass().clear();
-        AnchorePaneLayout.getStyleClass().clear();
-        GestionFactureButon.getStyleClass().clear();
-        GestionTechnicienButon.getStyleClass().clear();
-        GestionAffectationButon.getStyleClass().clear();
-        GestionPostButon.getStyleClass().clear();
-        GestionCommentButon.getStyleClass().clear();
 
-        gestionProduitButton.getStyleClass().add("btns");
-        GestionUserButon.getStyleClass().add("btns");
-        GestionCategorieButon.getStyleClass().add("btns");
-    //    GestionTicketButon.getStyleClass().add("btns");
-        gestionProduitButton.getStyleClass().add("btns");
-        GestionCategorieButon.getStyleClass().add("btns");
-     //   GestionCommandeButon.getStyleClass().add("btns");
-        AnchorePaneLayout.getStyleClass().add("btns");
-        GestionFactureButon.getStyleClass().add("btns");
-        GestionTechnicienButon.getStyleClass().add("btns");
-        GestionAffectationButon.getStyleClass().add("btns");
-        GestionPostButon.getStyleClass().add("btns");
-        GestionCommentButon.getStyleClass().add("btns");
-
-    }
 
     @FXML
     private void LoadGestionProduit(MouseEvent event) {
-       Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("afficherproduit.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Produit ");
-            gestionProduitButton.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("afficherproduit.fxml"));
+            gestionProduitButton.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     @FXML
     private void LoadGestionCategorie(MouseEvent event) {
-        Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("affichercat.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Catégorie ");
-            gestionProduitButton.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("affichercat.fxml"));
+            GestionCategorieButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
@@ -142,92 +85,68 @@ public class BaseController implements Initializable {
 
     @FXML
     private void LoadGestionPost(MouseEvent event) {
-        Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("PostBack.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Post");
-            GestionPostButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("postback.fxml"));
+            GestionPostButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     @FXML
     private void LoadGestionComment(MouseEvent event) {
-        Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("ShowCommentBack.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Comment");
-            GestionCommentButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("showcommentback.fxml"));
+            GestionCommentButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
         
     }
 
     @FXML
     private void LoadGestionFacture(MouseEvent event) {
-       Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("stat.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Facture");
-            GestionFactureButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("showcommentback.fxml"));
+            GestionCommentButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     @FXML
     private void LoadGestionTechnicien(MouseEvent event) {
-        Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("technicien.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Technicien");
-            GestionTechnicienButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("affichertechnicien.fxml"));
+            GestionTechnicienButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     @FXML
     private void LoadGestionAffectation(MouseEvent event) {
-         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("affectations.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion Affectation");
-            GestionAffectationButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("affectations.fxml"));
+            GestionAffectationButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     @FXML
     private void LoadGestionUser(MouseEvent event) {
-         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("alluser.fxml"));
-            removeSelectedStyle();
-            TitreLabel.setText("Gestion User");
-            GestionUserButon.getStyleClass().add("btn-selected");
-            AnchorePaneLayout.getChildren().removeAll();
-            AnchorePaneLayout.getChildren().setAll(root);
+            //navigation
+            Parent loader = FXMLLoader.load(getClass().getResource("alluser.fxml"));
+            GestionUserButon.getScene().setRoot(loader);
         } catch (IOException ex) {
-            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
     
